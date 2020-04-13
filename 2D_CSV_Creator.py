4e23b678d24838f36e4f5aef1da7920cc93bbaf6
starklp <62572353+starklp@users.noreply.github.com>
2020-04-13 16:38:56 -0400

    Update 2D_CSV_Creator.py
    This (in-progress) code is for running 2 arms.

--- 2D_CSV_Creator.py
+++ 2D_CSV_Creator.py
@@ -45,27 +45,40 @@ def trans_2d(ang_old, ang_new, x_offset, y_offset):
 ########################################################################
 dx_1 = 3.125
 dy_1 = 0
+dx_2 = 3.25
+dy_2 = 0
 ########################################################################
 def loop():
     while True:
         global results
         amount = range(0, 3)
         # with open('collect.csv', 'a', newline='\n') as f:
-        value_1 = analogRead(0)    # read the ADC value of channel 0
+        value_1 = analogRead(1)    # read the ADC value of channel 1
+        value_2 = analogRead(0)    # read the ADC value of channel 0
         # The bread board is running 3.3 Volts.  If all 3.3 Volts are running through the chip then the chip gives us 255 (and 0 at 0 volts).
         # If 300 degrees is our max rotation and 255 is our max value then the ratio between the two will allow us to get degrees (0.85).
-        deg_1 = (value_1 / 0.85) + 37.647
-        # Use matrix Functions to solve transformation
-        end_pos = rot(deg_1).dot(pos(dx_1, dy_1))
-        ###############################################
-        for i in amount:
-            if i == len(end_pos) - 1:
-                results += str(end_pos[i]).lstrip('[').rstrip(']')
+        deg_1 = round((value_1 / 0.85) + 37.647, 1)
+        deg_1_to_2 = round((value_2 / 0.85) + 129.412, 1)
+        if deg_1 >= 37.6 and deg_1 <= 337.6:
+            if deg_1_to_2 == 180:
+                deg_2 = deg_1
+            elif deg_1_to_2 > 180:
+                deg_2 = deg_1 + (deg_1_to_2 - 180)
             else:
-                results += str(end_pos[i]).lstrip('[').rstrip(']') + ','
-        results += '\n'
-        time.sleep(0.05)  # Pause for a second before next read
-        print(end_pos)
+                deg_2 = deg_1 - (180 - deg_1_to_2)
+        # Use matrix Functions to solve transformation
+        pos_2 = pos(3.25, 0)
+        pos_1 = trans_2d(deg_2, deg_1, dx_1, dy_1).dot(pos_2)
+        ###############################################
+        # for i in amount:
+            # if i == len(end_pos) - 1:
+                # results += str(end_pos[i]).lstrip('[').rstrip(']')
+            # else:
+                # results += str(end_pos[i]).lstrip('[').rstrip(']') + ','
+        # results += '\n'
+        print(deg_
+        time.sleep(0.5) # Pause for a second before next read
+
 
 
 def destroy():
